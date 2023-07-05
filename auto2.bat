17117af024c2b756e7875e6df37d30215861242f
Daeeun Lee <55821132+2daeeun@users.noreply.github.com>
2023-07-05 23:23:58 +0900

    [임시] 자동 설치 배치 파일2

---
+++ auto2.bat
@@ -0,0 +1,31 @@
+@echo off
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+choco feature enable -n=allowGlobalConfirmation
+pause
+
+:: 항상 자동으로 yes를 선택하도록 설정
+choco feature enable --name=allowGlobalConfirmation
+
+:: 먼저 기존 패키지 업데이트
+choco upgrade all -y
+
+:: 사용할 어플리케이션 설치
+choco install honeyview
+choco install kakaotalk
+choco install googlechrome
+choco install googledrive
+choco install sumatrapdf
+choco install potplayer
+choco install utorrent
+choco install office365business
+choco install barrier
+choco install virtualbox
+choco install neovim
+choco install vscode
+choco install visualstudio2022community
+choco install powershell-core
+choco install git.install -y --params "/GitAndUnixToolsOnPath /WindowsTerminal /WindowsTerminalProfile /NoGuiHereIntegration"
+git config --global color.ui auto
+
+:END
